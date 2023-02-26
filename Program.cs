c0ebcb5c14d4a084cc4e00f4d3f1c13fa8003b90
ZaripovT <timurfromcanada@gmail.com>
2023-02-26 21:05:29 +0500

    Starting of project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsMyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
